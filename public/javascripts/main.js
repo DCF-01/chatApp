4f1bc9ad724b81d38d29fe882f157e9c1492cdc5
DCF-01 <itxtechnologies.mk@gmail.com>
2021-02-26 13:57:35 +0100

    chat working, room id, list of users

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,14 +2,87 @@ const socket = io();
 const form = document.getElementById('chat-form');
 const msg_input = document.getElementById('msg');
 const single_msg = document.querySelector('.single-msg');
+const text = document.querySelector('.text');
+const time = document.querySelector('.time');
+const meta = document.querySelector('.meta');
 const chat_box = document.querySelector('.chat-box');
+const room_box = document.querySelector('.room-box');
+const users_list_box = document.querySelector('.users-list-box');
+const room_name = document.querySelector('.room-name');
 
-console.log('success');
+
+// get url strings
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
+
+socket.emit('joinRoom', { username, room});
+
+function createMessageElement(username, text, time){
+    let el = document.createElement('div');
+    let p_1 = document.createElement('p');
+    let p_2 = document.createElement('p');
+
+    //add classnames
+    el.className = 'single-msg';
+    p_1.className = 'meta';
+    p_2.className = 'text';
+
+    //append to parents
+    el.appendChild(p_1);
+    el.appendChild(p_2);
+    
+
+    p_1.innerHTML= username + ' - ' +`<span class="time"> ${time}`;
+    p_2.textContent=text;
+    
+    //append to chat box
+    chat_box.appendChild(el);
+    
+    chat_box.scrollTo(0, chat_box.scrollHeight);
+}
+
+function clearDomTree(){
+    let arr = Array.from(users_list_box.childNodes);
+    arr.forEach(element => {
+        users_list_box.removeChild(element);
+    });
+}
+
+function updateUsersList(users){
+    clearDomTree();
+    console.log(users)
+
+    users.forEach(el => {
+        let users_list_single = document.createElement('p');
+        users_list_single.className = 'users-list-single';
+        users_list_single.textContent = el.username;
+        users_list_box.appendChild(users_list_single);
+    });
+}
+
+function updateRoomName(room){
+    room_name.textContent = room;
+}
+
+
+
+
+socket.on('roomInfo', ({room, users}) => {
+    updateRoomName(room);
+    updateUsersList(users);
+})
+
 
 socket.on('message', message => {
-    const new_msg = single_msg.cloneNode(false);
-    new_msg.textContent = message;
-    chat_box.appendChild(new_msg);
+    console.log(message);
+    // const new_msg = single_msg.cloneNode(true);
+    createMessageElement(
+        message.username,
+        message.text, 
+        message.time
+    );
+    
 
     //clear input field and focus
     msg_input.value = '';
